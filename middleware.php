fc8cc4c2ce17327f2233f959dc8a1691c79dead9
Velimir Efremovski unknown student <ve23@afsconnect2.njit.edu>
2016-03-04 14:19:13 -0500

    update 2

--- middleware.php
+++ middleware.php
@@ -1,30 +1,21 @@
 <?php
 
-//input from frontend + decode with assigned variables
+//input from frontend 
 
-$jsonstr=$_POST['name'];
-$recieve = json_decode($jsonstr, true);
+$jsonstr = $_POST['name'];
+$jsonstr1 = $_POST['pwd'];
+
+//echo json_encode(array('valid' => true));
 
 
-echo json_encode(array('valid' => true)); 
-
-
-$ucid = $recieve["ucid"];
-$password = $recieve["password"];
-
-//array for njit and db
-$output = array();
-$dba = array("ucid" => $ucid, "password" => $password);
-$njit = array("ucid" => $ucid, "password" => $password);
-
-//curl posts
+//db curl posts
 
 $ch1 = curl_init();
 $ch2 = curl_init();
 
 curl_setopt($ch1, CURLOPT_URL, "https://web.njit.edu/~rs334/cs490/index.php");
 curl_setopt($ch1, CURLOPT_POST, true);
-curl_setopt($ch1, CURLOPT_POSTFIELDS, json_encode($dba));
+curl_setopt($ch1, CURLOPT_POSTFIELDS, json_encode(array('username'=>"Ve23",'password'=>"cs490"));
 curl_setopt($ch1, CURLOPT_RETURNTRANSFER, 1);
 
 $dbconresults = curl_exec($ch1);
@@ -33,16 +24,19 @@ curl_close($ch1);
 
 //njit curl posts
 
-curl_setopt($ch2, CURLOPT_URL, "https://www.webauth.njit.edu/idp/Authn/UserPassword");
+curl_setopt($ch2, CURLOPT_URL,"https://www.webauth.njit.edu/idp/Authn/UserPassword?");
 curl_setopt($ch2, CURLOPT_POST, true);
-curl_setopt($ch2, CURLOPT_POSTFIELDS, json_encode($njit));
+curl_setopt($ch2, CURLOPT_POSTFIELDS, json_encode(array('ucid'=>"ve23", 'pass'=>"csstudent"));
 curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
+//echo "build success"; 
 
 $njitconresults = curl_exec($ch2);
 $output["njit"] = $njitconresults;
 curl_close($ch2);
 
-echo json_encode($output); 
+
+//echo json_encode($output);
+echo json_encode(array('UCID'=>"Ve23", 'pwd'=>"cs490"));
 
 
 ?>
