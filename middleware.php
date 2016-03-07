5d1ba020e0abbdacf5b528bde967fe5485cd8cbd
Velimir Efremovski unknown student <ve23@afsconnect2.njit.edu>
2016-03-07 15:30:03 -0500

    works with database but njit is whack

--- middleware.php
+++ middleware.php
@@ -6,37 +6,38 @@ $jsonstr = $_POST['name'];
 $jsonstr1 = $_POST['pwd'];
 
 //echo json_encode(array('valid' => true));
-
-
+$data = array("uid"=>"rs334", "password"=>"password");
+$njit = array("ucid"=>"ve23", "password"=>"wrong");
+$string = http_build_query($data);
+$string2 = http_build_query($njit);
 //db curl posts
 
-$ch1 = curl_init();
-$ch2 = curl_init();
+$ch1 = curl_init("https://web.njit.edu/~rs334/cs490/data.php");
+$ch2 = curl_init("https://www.njit.edu/cp/login.php");
 
-curl_setopt($ch1, CURLOPT_URL, "https://web.njit.edu/~rs334/cs490/index.php");
 curl_setopt($ch1, CURLOPT_POST, true);
-curl_setopt($ch1, CURLOPT_POSTFIELDS, json_encode(array('username'=>"Ve23",'password'=>"cs490"));
-curl_setopt($ch1, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch1, CURLOPT_POSTFIELDS, $string);
+curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
 
-$dbconresults = curl_exec($ch1);
-$output["database"] = $dbconresults;
+$dbres = curl_exec($ch1);
+echo $dbres;
+//echo json_encode(array('uid'=>"ve23", 'password'=>"cs490"));
 curl_close($ch1);
 
 //njit curl posts
 
-curl_setopt($ch2, CURLOPT_URL,"https://www.webauth.njit.edu/idp/Authn/UserPassword?");
 curl_setopt($ch2, CURLOPT_POST, true);
-curl_setopt($ch2, CURLOPT_POSTFIELDS, json_encode(array('ucid'=>"ve23", 'pass'=>"csstudent"));
-curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch2, CURLOPT_POSTFIELDS, $njit);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
 //echo "build success"; 
 
-$njitconresults = curl_exec($ch2);
-$output["njit"] = $njitconresults;
+$njitres = curl_exec($ch2);
+echo $njitres;
 curl_close($ch2);
 
 
 //echo json_encode($output);
-echo json_encode(array('UCID'=>"Ve23", 'pwd'=>"cs490"));
+echo json_encode(array('ucid'=>"Ve23", 'pwd'=>"redline16k"));
 
 
 ?>
