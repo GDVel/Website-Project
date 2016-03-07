a74a3148f5964f8fbcb31db08c9235856cb01f07
Velimir Efremovski unknown student <ve23@afsconnect2.njit.edu>
2016-03-07 17:19:28 -0500

    db works, njit possibly works?

--- middleware.php
+++ middleware.php
@@ -8,7 +8,7 @@ $jsonstr1 = $_POST['pwd'];
 //:exit();
 //echo json_encode(array('valid' => true));
 $data = array("uid"=> $jsonstr, "password"=> $jsonstr1);
-$njit = array("ucid"=>"ve23", "password"=>"redline16k", "uuid"=>'0xACA021');
+$njit = array("user"=> $jsonstr, "pass"=> $jsonstr1, "uuid"=>'0xACA021');
 $string = http_build_query($data);
 $string2 = http_build_query($njit);
 //$role = array('ucid'=> "student", 'njit'=>"is njit student", 'noauth'=> "0");
@@ -24,24 +24,21 @@ curl_setopt($ch1, CURLOPT_POSTFIELDS, $string);
 curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
 
 $dbres = curl_exec($ch1);
-$meow=!($dbres=="not authenticated");
-//echo $meow;
-$res = array('isDB'=> $meow , 'roles'=> $dbres, 'isNJIT'=> false); 
-echo json_encode($res);
+
 //echo json_encode(array('uid'=>"ve23", 'password'=>"cs490"));
 curl_close($ch1);
 
 //njit curl posts
 
-//$ch2 = curl_init("https://www.njit.edu/cp/login.php");
+$ch2 = curl_init("https://cp4.njit.edu/cp/home/login");
 
 curl_setopt($ch2, CURLOPT_POST, true);
-curl_setopt($ch2, CURLOPT_POSTFIELDS, $njit);
+curl_setopt($ch2, CURLOPT_POSTFIELDS, $string2);
 curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
 //echo "build success"; 
 
 $njitres = curl_exec($ch2);
-echo $njitres;
+//echo $njitres;
 curl_close($ch2);
 
 
@@ -49,6 +46,10 @@ curl_close($ch2);
 //echo json_encode(array('ucid'=>"Ve23", 'pwd'=>"wrong"));
 //echo json_encode($dbres);
 
+$meow=!($dbres=="not authenticated");
+//echo $meow;
+$res = array('isDB'=> $meow , 'roles'=> $dbres, 'isNJIT'=> false);
+echo json_encode($res);
 
 ?>
 
