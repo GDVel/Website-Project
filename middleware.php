79e15d7a38f84a9989b9ce3376bfc62ee2bddcf1
GodDamnVel <ve23@njit.edu>
2016-03-28 15:10:19 -0400

    Update middleware.php

--- middleware.php
+++ middleware.php
@@ -4,14 +4,12 @@
 
 $jsonstr = $_POST['name'];
 $jsonstr1 = $_POST['pwd'];
-//echo json_encode(array('ucid'=>$jsonstr, 'pwd'=>$jsonstr1));
-//:exit();
-//echo json_encode(array('valid' => true));
+
 $data = array("uid"=> $jsonstr, "password"=> $jsonstr1);
 $njit = array("user"=> $jsonstr, "pass"=> $jsonstr1, "uuid"=>'0xACA021');
 $string = http_build_query($data);
 $string2 = http_build_query($njit);
-//$role = array('ucid'=> "student", 'njit'=>"is njit student", 'noauth'=> "0");
+
 
 
 //db curl posts
@@ -25,7 +23,7 @@ curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
 
 $dbres = curl_exec($ch1);
 
-//echo json_encode(array('uid'=>"ve23", 'password'=>"cs490"));
+
 curl_close($ch1);
 
 //njit curl posts
@@ -38,16 +36,12 @@ curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
 //echo "build success"; 
 
 $njitres = curl_exec($ch2);
-//echo $njitres;
+
 curl_close($ch2);
 
 
-//echo json_encode($output);
-//echo json_encode(array('ucid'=>"Ve23", 'pwd'=>"wrong"));
-//echo json_encode($dbres);
-
 $meow=!($dbres=="not authenticated");
-//echo $meow;
+
 $res = array('isDB'=> $meow , 'roles'=> $dbres, 'isNJIT'=> false);
 echo json_encode($res);
 
